aed8f6f1d7a04e3989813c98429b85c92f2fea0a
Anshul Kumar <anshulkashyap118@gmail.com>
2026-07-23 14:33:59 +0530

    Time: 10 ms (55.68%) | Memory: 47.9 MB (59.73%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -10,7 +10,7 @@ class Solution {
                 high = b;
             }
         }
-        int ans = high;
+    
         while(low<=high)
         {
             int mid = low+(high-low)/2;
@@ -18,18 +18,17 @@ class Solution {
            long sum = 0;
             for(int b :piles)
             {
-                sum += (b+mid-1)/mid;
+                sum += ((long)b+mid-1)/mid;
             }
             if(sum<=h)
             {
-                ans = mid;
-
+                
                 high = mid-1;
             }else{
                 low = mid+1;
             }
 
         }
-        return ans;
+        return low;
     }
 }
